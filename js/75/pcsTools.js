f448d88a7f66fdcab57e69d751854756ea1ea6e1
slubowsky <270589+slubowsky@users.noreply.github.com>
2022-09-18 22:52:18 -0400

    class 75

---
+++ js/75/pcsTools.js
@@ -0,0 +1,53 @@
+window.app = window.app || {};
+window.app.pcsTools = (function () {
+  'use strict';
+
+  function get(selector) {
+    return document.querySelector(selector);
+  }
+
+  function setCss(elem, property, value) {
+    //console.log('in set with', property, value);
+    elem.style[property] = value;
+  }
+
+  function getCss(elem, property) {
+    //console.log('in get with', property);
+    return elem.style[property];
+  }
+
+  function addEventListener(elem, type, callback) {
+    elem.addEventListener(type, callback);
+  }
+
+  return {
+    wrap: function (selector) {
+      const elem = get(selector);
+
+      return {
+        /*setCss: (property, value) => setCss(elem, property, value),
+        getCss: (property) => getCss(elem, property)*/
+        css: function (property, value) {
+          if (arguments.length === 2) {
+            setCss(elem, property, value);
+            return this;
+          } else {
+            return getCss(elem, property);
+          }
+        },
+        click: function(callback)  {
+          addEventListener(elem, 'click', callback);
+          return this;
+        },
+        hide: function() {
+          setCss(elem, 'display', 'none');
+          return this;
+        },
+        show: function(displayValue = 'block') {
+          setCss(elem, 'display', displayValue);
+          return this;
+        }
+      };
+    }
+  };
+})();
