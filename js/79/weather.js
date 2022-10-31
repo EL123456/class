9b007b83c548e7444da275b04b5b8f06d382597b
slubowsky <270589+slubowsky@users.noreply.github.com>
2022-10-30 22:42:55 -0400

    remove api key

--- js/79/weather.js
+++ js/79/weather.js
@@ -1,14 +1,10 @@
-/*77834973e8caa524a142bcea4a68e1b8
-
-https://api.openweathermap.org/data/2.5/weather?zip=08701&appid=77834973e8caa524a142bcea4a68e1b8
-
-https://api.openweathermap.org/data/2.5/weather?zip=08701&appid=77834973e8caa524a142bcea4a68e1b8&units=imperial&lang=he*/
-
 (function () {
   'use strict';
 
+  const key = 'get your own key - but if you must you can use this'; //4d940566413cbb48ddbe156f2b502364';
+
   async function getWeather(zip) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=77834973e8caa524a142bcea4a68e1b8&units=imperial&lang=he`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${key}&units=imperial&lang=he`);
     const weatherData = await response.json();
     console.log(weatherData);
   }
